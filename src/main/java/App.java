8ed641b72543f3aa8cd36ab815e2f09be372bc13
Keith Stedman <keithstedman@gmail.com>
2017-04-07 15:01:13 -0700

    testing sightings

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,6 +15,7 @@ public class App {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("animals", Animal.all());
       model.put("rangers", Ranger.all());
+      model.put("locations", Location.all());
       //model.put("sightings", Sighting.all());
       model.put("endangeredAnimals", EndangeredAnimal.all());
       model.put("sightings-report", Sighting.all());
@@ -23,6 +24,7 @@ public class App {
       model.put("all-animals", "templates/all-animals.vtl");
       model.put("all-rangers", "templates/all-rangers.vtl");
       model.put("all-sightings", "templates/all-sightings.vtl");
+      model.put("all-locations", "templates/all-locations.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
@@ -95,6 +97,7 @@ public class App {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("animals", Animal.all());
       model.put("rangers", Ranger.all());
+      model.put("locations", Location.all());
       model.put("endangeredAnimals", EndangeredAnimal.all());
       model.put("header", "templates/header.vtl");
       model.put("sightings", "templates/sightings.vtl");
@@ -102,14 +105,14 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/new-sighting", (request, response) -> {
+    post("/sighting/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       int ranger_id = Integer.parseInt(request.queryParams("rangerSelected"));
       int animal_id = Integer.parseInt(request.queryParams("animalSelected"));
       int location_id = Integer.parseInt(request.queryParams("location"));
       Sighting sighting = new Sighting(animal_id, location_id, ranger_id);
       sighting.save();
-      response.redirect("/ranger/ranger_id");
+      response.redirect("/");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
