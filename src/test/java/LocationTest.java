884b6232b7b759fb6d227890b54a6685e472da4b
Keith Stedman <keithstedman@gmail.com>
2017-04-07 14:34:37 -0700

    more location testing

--- src/test/java/LocationTest.java
+++ src/test/java/LocationTest.java
@@ -5,12 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 
-public class RangerTest {
+public class LocationTest {
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
   @Test
   public void ranger_instantiatesCorrectly_ture() {
-    Ranger testRanger = new Ranger("Dave");
-    assertEquals(true, testRanger instanceof Ranger);
+    Location testLocation = new Location("River Bank");
+    assertEquals(true, testLocation instanceof Location);
   }
+
+  @Test
+  public void getName_animalInstantiatesWithName_Deer() {
+    Ranger testRanger = new Ranger("Dave");
+    assertEquals("Dave", testRanger.getName());
+  }
+
+}
